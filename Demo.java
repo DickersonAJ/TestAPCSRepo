7bb2c95b9f5348eef8d47b007aea676d45931d6c
DickersonAJ <adickerson10@stuy.edu>
2020-10-20 17:38:06 -0400

    I tested this program and it works now

---
+++ Demo.java
@@ -0,0 +1,22 @@
+public class Demo {
+
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      printLoop(5);
+    }
+    else {
+      int n = Integer.parseInt(args[0]);
+      printLoop(n);
+    }
+  }
+
+  public static void printLoop(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j <= n - i; j++ ) {
+        System.out.print(i);
+      }
+      System.out.println();
+    }
+  }
+
+}
